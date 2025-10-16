84b10083e1563e021043016ac2522ba5c601de74
Esther Arias <eariasvalor@gmail.com>
2025-10-16 12:42:20 +0200

    fix: separated classes into a commont package for convenience

---
+++ src/main/java/common_classes/SampleMonths.java
@@ -0,0 +1,24 @@
+package common_classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SampleMonths {
+    public static List<String> getMonths(){
+        List<String> names = new ArrayList<>();
+        names.add("January");
+        names.add("February");
+        names.add("March");
+        names.add("April");
+        names.add("May");
+        names.add("June");
+        names.add("July");
+        names.add("August");
+        names.add("September");
+        names.add("October");
+        names.add("November");
+        names.add("December");
+
+        return names;
+    }
+}
---
+++ src/main/java/common_classes/SampleNames.java
@@ -0,0 +1,18 @@
+package common_classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SampleNames {
+    public static List<String> getNames(){
+        List<String> names = new ArrayList<>();
+        names.add("Marcos");
+        names.add("José");
+        names.add("Elena");
+        names.add("Olga");
+        names.add("Natalia");
+
+        return names;
+    }
+}
+
