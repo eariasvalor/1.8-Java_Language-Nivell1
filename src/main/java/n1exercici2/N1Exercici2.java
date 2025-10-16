e2abcc323dd47c95e7b6913b1d1bbba07934122d
Esther Arias <eariasvalor@gmail.com>
2025-10-16 12:42:55 +0200

    fix: minor fixes (method naming)

---
+++ src/main/java/n1exercici2/N1Exercici2.java
@@ -0,0 +1,18 @@
+package n1exercici2;
+
+import common_classes.SampleNames;
+
+import java.util.List;
+
+public class N1Exercici2 {
+    public static void main(String[] args){
+        List<String> names = SampleNames.getNames();
+        Filter filter = new Filter();
+        List<String> filteredList = filter.filterByLetter(names, "o");
+
+        filteredList.forEach(System.out::println);
+
+
+    }
+}
+
