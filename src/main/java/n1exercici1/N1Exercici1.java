932d7ea4837a3c2a79a3089358d8684d76323171
Esther Arias <eariasvalor@gmail.com>
2025-10-16 10:41:37 +0200

    feat: create List and Lambda expression

--- src/main/java/n1exercici1/N1Exercici1.java
+++ src/main/java/n1exercici1/N1Exercici1.java
@@ -1,7 +1,26 @@
 package n1exercici1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class N1Exercici1 {
     public static void main(String[] args){
 
+        List<String> names = new ArrayList<>();
+        names.add("Marcos");
+        names.add("José");
+        names.add("Elena");
+        names.add("Olga");
+        names.add("Natalia");
+
+        IStringsFilter filter = () -> {
+            for (String s : names) {
+                if (s.toLowerCase().contains("o")) {
+                    System.out.println(s);
+                }
+            }
+        };
+
+        filter.filter();
     }
 }
