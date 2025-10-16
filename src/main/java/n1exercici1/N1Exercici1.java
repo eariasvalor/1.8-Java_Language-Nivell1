faae2c4a62ee5b70762c4c3546fb18ed16e8381c
Esther Arias <eariasvalor@gmail.com>
2025-10-16 12:42:37 +0200

    fix: minor fixes

--- src/main/java/n1exercici1/N1Exercici1.java
+++ src/main/java/n1exercici1/N1Exercici1.java
@@ -1,28 +1,18 @@
 package n1exercici1;
 
-import java.util.ArrayList;
+import common_classes.SampleNames;
+
 import java.util.List;
 
 public class N1Exercici1 {
     public static void main(String[] args){
 
-        List<String> names = new ArrayList<>();
-        names.add("Marcos");
-        names.add("José");
-        names.add("Elena");
-        names.add("Olga");
-        names.add("Natalia");
-
+        List<String> names = SampleNames.getNames();
 
         Filter filter = new Filter();
         List<String> filteredList = filter.filterByLetter(names, "o");
 
         filteredList.forEach(System.out::println);
-
-
-
-
-
 
     }
 }
---
+++ src/main/java/n1exercici2/Filter.java
@@ -0,0 +1,18 @@
+package n1exercici2;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Filter {
+    public List<String> filterByLetter(List<String> list, String letter){
+        if (list == null || letter == null || letter.isEmpty()) {
+            throw new IllegalArgumentException("The list or the letter cannot be empty.");
+        }
+        String lowerLetter = letter.toLowerCase();
+        return list.stream()
+                .filter(s -> s.toLowerCase().contains(lowerLetter))
+                .filter(s -> s.length() > 5)
+                .collect(Collectors.toUnmodifiableList());
+
+    }
+}
